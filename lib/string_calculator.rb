255018a3be78b5c203274184ce7bde0132058078
alex <alexander.mellor@gmail.com>
2016-09-09 13:22:58 +0100

    Refactor non default delimiter to remove if else statement

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,6 @@
 class StringCalculator
   def add(string_number)
-    if string_number.start_with?("//")
-        string_number_array = string_number.gsub(";", ",").split(',')
-      else
-        string_number_array = string_number.gsub("\n", ",").split(',')
-    end
+    string_number_array = string_number.gsub(/\W/, ",").split(',')
     string_number_array.map(&:to_i).reduce(0, :+)
   end
 end
