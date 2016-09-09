de44a45c7c8aaae8ac7ab1d62c80d53bdad70142
alex <alexander.mellor@gmail.com>
2016-09-09 15:46:18 +0100

    Refactor converting string to array into own method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,15 @@
 class StringCalculator
   def add(string_number)
-    new_string = extract_delimiter(string_number).split(',')
-    new_string.map(&:to_i).reduce(0, :+)
+    new_string = extract_delimiter(string_number)
+    new_string_array = convert_string_to_array(new_string)
+    new_string_array.map(&:to_i).reduce(0, :+)
   end
 
   def extract_delimiter(string)
     string.gsub(/\W/, ",")
+  end
+
+  def convert_string_to_array(string)
+    string.split(',')
   end
 end
