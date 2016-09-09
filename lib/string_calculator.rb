cfd4d0f6d8cbef3c525069c4a08ab830256cd4fd
alex <alexander.mellor@gmail.com>
2016-09-09 15:40:50 +0100

    Refactor extracting the delimiter to own method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,10 @@
 class StringCalculator
   def add(string_number)
-    string_number_array = string_number.gsub(/\W/, ",").split(',')
+    string_number_array = extract_delimiter(string_number).split(',')
     string_number_array.map(&:to_i).reduce(0, :+)
+  end
+
+  def extract_delimiter(string)
+    string.gsub(/\W/, ",")
   end
 end
