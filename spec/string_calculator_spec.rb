8c831b5bae98af35b3d8943fc882062f404ad225
alex <alexander.mellor@gmail.com>
2016-09-09 12:21:51 +0100

    Refactor tests into context blocks

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,24 +2,28 @@ require "string_calculator"
 
 describe StringCalculator do
   describe "#add" do
-    it "returns zero if passed an empty string" do
-      expect(subject.add("")).to eq(0)
+    context "when default delimiter is used" do
+      it "returns zero if passed an empty string" do
+        expect(subject.add("")).to eq(0)
+      end
+
+      it "returns 1 if passed 1" do
+        expect(subject.add("1")).to eq(1)
+      end
+
+      it "returns 3 when passed 1,2" do
+        expect(subject.add("1,2")).to eq(3)
+      end
+
+      it "returns 15 when passed 1,2,3,4,5" do
+        expect(subject.add("1,2,3,4,5")).to eq(15)
+      end
     end
 
-    it "returns 1 if passed 1" do
-      expect(subject.add("1")).to eq(1)
-    end
-
-    it "returns 3 when passed 1,2" do
-      expect(subject.add("1,2")).to eq(3)
-    end
-
-    it "returns 15 when passed 1,2,3,4,5" do
-      expect(subject.add("1,2,3,4,5")).to eq(15)
-    end
-
-    it "returns 6 when passed 1\n2,3" do
-      expect(subject.add("1\n2,3")).to eq(6)
+    context "when non-default delimiter is used" do
+      it 'returns 6 when passed 1\n2,3' do
+        expect(subject.add("1\n2,3")).to eq(6)
+      end
     end
   end
 end
