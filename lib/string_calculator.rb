21bb3f974d606fe559c1bd115b7355479519e58c
alex <alexander.mellor@gmail.com>
2016-09-09 11:18:43 +0100

    Refactor add method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,6 @@
 class StringCalculator
   def add(string_number)
     string_number_array = string_number.split(',')
-    string_number_array[0].to_i + string_number_array[1].to_i
+    string_number_array.map(&:to_i).reduce(0, :+)
   end
 end
