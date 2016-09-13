576ecf2f05622d427718263224fd0502de64eaf7
alex <alexander.mellor@gmail.com>
2016-09-13 18:13:24 +0100

    Renamed variables in add method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,7 @@
 class StringCalculator
   def add(string_number)
-    new_string_array = convert_string_to_array(string_number)
-    integer_array = convert_to_integer(new_string_array)
+    string_array = convert_string_to_array(string_number)
+    integer_array = convert_to_integer(string_array)
     sum_array(integer_array)
   end
 
